cb08be2a2f9a3b9dfad3cf28dce0419e49a63c9f
Alex Teisheira <fukirx@gmail.com>
2020-11-27 13:20:58 +0200

    test url fix

--- controllers/postTestController.js
+++ controllers/postTestController.js
@@ -1,53 +1,48 @@
-const testService = require('../services/testService')
-
+const testService = require("../services/testService");
 
 const testSave = async (ctx, next) => {
-    
-    const { body: { questions, ...testData } } = ctx.request
+  const {
+    body: { questions, ...testData },
+  } = ctx.request;
 
-    ctx.state.questions = questions
+  ctx.state.questions = questions;
 
-    try {
-        const testObj = await testService.saveTest(testData)
-        ctx.state.test = testObj
+  try {
+    const testObj = await testService.saveTest(testData);
+    ctx.state.test = testObj;
 
-        return await next()
-
-    } catch (e) {
-        throw new Error(`Error in the TestController->testSave: ${e}`)
-    }
-}
+    return await next();
+  } catch (e) {
+    throw new Error(`Error in the TestController->testSave: ${e}`);
+  }
+};
 
 const questionsSave = async (ctx, next) => {
+  const { questions, test } = ctx.state;
 
-    const { questions, test } = ctx.state
+  try {
+    await Promise.all(
+      questions.map(async (q) => {
+        const question = await testService.saveQuestion(q);
+        return await question.setTest(test);
+      })
+    );
 
-    try {
-        await Promise.all(questions.map(async q => {
-
-            const question = await testService.saveQuestion(q)
-            return await question.setTest(test)
-        }))
-
-        return next()
-
-    } catch (e) {
-        throw new Error(`Error in the TestController->testQuestions: ${e}`)
-    }
-}
+    return next();
+  } catch (e) {
+    throw new Error(`Error in the TestController->testQuestions: ${e}`);
+  }
+};
 
 const resDispatch = async (ctx) => {
+  const { request, response } = ctx;
+  const { test } = ctx.state;
 
-    const {request, response} = ctx
-    const {test} = ctx.state
+  response.status = 200;
 
-    response.status = 200
+  response.body = {
+    testUrl: `${request.header.origin}/testify/test/${test.id}`,
+  };
+};
 
-    response.body = {
-        testUrl: `${request.header.origin}/test/${test.id}`
-    }
-
-   
-}
-
-module.exports = { testSave, questionsSave, resDispatch }
+module.exports = { testSave, questionsSave, resDispatch };
